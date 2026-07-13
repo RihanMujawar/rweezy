0f1820c610a9f4729889195187818304d6009a84
Rehan <rehanmujawarofficial007@gmail.com>
2026-07-13 13:58:00 +0530

    cleand the workspace

--- backend/test-prisma.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
-
-prisma.user.findUnique({ where: { id: 'test' } })
-  .then(() => console.log('Query works!'))
-  .catch(e => console.error('Error:', e.message))
-  .finally(() => prisma.$disconnect());
